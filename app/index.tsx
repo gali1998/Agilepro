cfcfcf6cf52b676599ac49c4917c124bc29d810a
Unknown <gali1998@gmail.com>
2019-05-01 23:48:07 +0300

    index files

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Header } from "./Components/Header/Header";
+
+ReactDOM.render(
+    <Header/>,
+    document.getElementById("app")
+);
