41e5d0f937bba41ab262ec7e94c71b8c887773fc
Unknown <gali1998@gmail.com>
2019-05-01 23:49:17 +0300

    Header component

---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const Header = () => { return (
+<div className="header">
+    <div className="logo"></div>
+    <ul>
+        <li>About</li>
+        <li>Contact us</li>
+    </ul>
+</div>)
+};
