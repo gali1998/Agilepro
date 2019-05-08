857d66da45983f35e4769d2bea9e8235efc4a439
Unknown <gali1998@gmail.com>
2019-05-09 00:42:22 +0300

    about + contact us components

---
+++ app/Components/About/About.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const About = () => { return (
+<div className="about">
+<h2>About</h2>
+    <p>
+      This app uses React!
+    </p>
+</div>)
+};
---
+++ app/Components/ContactUs/ContactUs.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {Mail} from "../Mail/Mail";
+
+export const ContactUs = () => { return (
+<div className="contactUs">
+<h2>Contact Us</h2>
+    <Mail/>
+</div>)
+};
